cabb4aa200ce1fbede4e44c07352eaeddbc14074
giovanamacedo <giovanaml12@gmail.com>
2023-12-19 00:17:41 -0300

    estudando controles de fluxo condicionais

---
+++ 04 - controle de fluxo/switchcase.js
@@ -0,0 +1,14 @@
+let funcao = 'Moderador';
+
+switch (funcao) {
+  case 'Convidado':
+    console.log('Usuário convidado');
+    break;
+  
+  case 'Moderador':
+    console.log('Usuário moderador');
+    break;
+
+  default:
+    console.log('Usuário desconhecido');
+}
