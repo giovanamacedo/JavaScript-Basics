bb5e6e4789c0dc4ce5ae29c7fc841143330ea4ce
giovanamacedo <giovanaml12@gmail.com>
2023-12-19 23:59:05 -0300

    estudando laços de repetição

---
+++ 04 - controle de fluxo/for.js
@@ -0,0 +1,10 @@
+//for
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+
+for (let i = 0; i <= 5; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+}
---
+++ 04 - controle de fluxo/while.js
@@ -0,0 +1,17 @@
+//while
+let i = 0;
+while (i <= 5) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+  i++;
+}
+
+//do while
+let x = 0;
+do {
+  if (x % 2 !== 0) {
+    console.log(x);
+  }
+  x++;
+} while (x <= 5);
